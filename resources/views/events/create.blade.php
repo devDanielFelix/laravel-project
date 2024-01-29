1c7ffc761f639827fa24fd245f28eea3957b0869
devDanielFelix <danielfelix72@hotmail.com>
2024-01-29 13:48:10 -0300

    json

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -32,6 +32,22 @@
             <label for="title">Descrição:</label>
             <textarea name="description" id="description" class="form-control" placeholder="O que vai acontercer no evento"></textarea>
         </div>
+        <div class="form-group">
+            <label for="title">Itens de infraestrutura:</label>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Cadeiras"> Cadeiras
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Brindes"> Brindes
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Open Food"> Open Food
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Palco"> Palco
+            </div>
+        </div>
+
         <input type="submit" class="btn btn-primary" value="Criar Evento">
     </form>
 </div>
